74564de450817b27be13134865e6f1c8e8e4ccda
Mohamed Elsayed <m.elsayed@advantive.net>
2022-01-08 20:55:04 +0200

    fix tls

--- fixPermissions.sh
+++ fixPermissions.sh
@@ -1,8 +1,12 @@
 find . -type f -exec chmod 644 {} \;
 find . -type d -exec chmod 755 {} \;
 chmod +x fixPermissions.sh 
-chmod -R 777 app/tmp
+mkdir -p app/tmp/pdf
 mkdir -p app/webroot/img/upload
 mkdir -p app/webroot/files/upload
+mkdir -p app/webroot/files/admissions
+chmod -R 777 app/tmp
+chmod -R 777 app/tmp/pdf
 chmod -R 777 app/webroot/img/upload
 chmod -R 777 app/webroot/files/upload
+chmod -R 777 app/webroot/files/admissions
