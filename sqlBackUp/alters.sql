accfbf3c2d9727404e71e4c3d74c2df2d42f7668
Mohamed Elsayed <m.elsayed@advantive.net>
2022-01-08 02:02:58 +0200

    make admission form works only

--- sqlBackUp/alters.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `team_members` CHANGE `linkedin` `linkedin` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
-ALTER TABLE `users` CHANGE `group_id` `group_id` INT(11) NOT NULL DEFAULT '0';
-ALTER TABLE `settings` ADD `vacancies_url` VARCHAR(255) NULL AFTER `carrers_developments_paging_limit`;
