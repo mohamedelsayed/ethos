405b6c66d16f05249ffe0e6add455f9cdab0d02f
Mohamed Elsayed <m.elsayed@advantive.net>
2022-01-08 21:42:16 +0200

    fix tls

--- app/controllers/requests_controller.php
+++ app/controllers/requests_controller.php
@@ -430,7 +430,7 @@ class RequestsController extends AuthController {
             ]);
             $dompdf->setHttpContext($context);
             $style = new \Dompdf\Css\Stylesheet($dompdf);
-            $style->load_css_file($base_url . '/css/backend/admissions_pdf.css');
+            // $style->load_css_file($base_url . '/css/backend/admissions_pdf.css');
             $dompdf->setCss($style);
             $html .= '<html>
                     <head>
