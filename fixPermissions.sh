3267de1c7edc6fed687ab8cd1bd9aeb5b95f3452
Mohamed Elsayed <m.elsayed@advantive.net>
2022-01-08 02:07:14 +0200

    make admission form works only

--- fixPermissions.sh
+++ fixPermissions.sh
@@ -2,6 +2,7 @@ find . -type f -exec chmod 644 {} \;
 find . -type d -exec chmod 755 {} \;
 chmod +x fixPermissions.sh 
 chmod -R 777 app/tmp
+mkdir -p app/webroot/img/upload
+mkdir -p app/webroot/files/upload
 chmod -R 777 app/webroot/img/upload
 chmod -R 777 app/webroot/files/upload
-
