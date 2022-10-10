6f6b0279afcc752f3d2af9a840dff4894b215f26
Mohamed Elsayed <me@mohamedelsayed.net>
2022-10-11 00:17:14 +0200

    add I am a Rukanian option in how_did_you_hear_about_us dropdown

--- app/config/constants.php
+++ app/config/constants.php
@@ -6,6 +6,6 @@
     'child_in_ethos' => 'I already have a child in Ethos',
     'staff_member' => 'I’m a staff member',
     'from_rukan' => 'From Rukan',
-    'I am a Rukanian' => 'I am a Rukanian',
+    'rukanian' => 'I am a Rukanian',
     'other' => 'Other',
 ];
